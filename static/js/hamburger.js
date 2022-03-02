e672477c985f8eb047f82996f3385ae9ce1eb886
Rishabh Kaushick <rishabh.kaushick@gmail.com>
2022-03-02 23:56:07 +0530

    Added common html logic in hamburger.js and respectively updated create.html and report.html

---
+++ static/js/hamburger.js
@@ -0,0 +1,16 @@
+document.write(`
+<link rel="icon" type="image/x-icon" href="../static/images/milk_1.png">
+<link rel="stylesheet" type="text/css" href="../static/css/home_css.css">
+<script src="../static/js/script.js"> </script>
+
+<span style="font-size:30px;cursor:pointer; color:white; " onclick="openNav()">&#9776;</span>
+<h1>PROJECT DOODH</h1>
+
+ <div id="mySidenav" class="sidenav">
+  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+  <a href="report">Report</a>
+  <a href="default">Default Milk</a>
+  <a href="#" style="font-size:21px">Change past day milk quantity</a>
+  </div>
+`);
+
