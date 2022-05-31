98f3a94aeee0d5aa2319a46caa7cae8efe66ea17
Rishabh Kaushick <rishabh.kaushick@gmail.com>
2022-05-31 14:16:55 +0530

    Worked on making the UI responsive for all pages.

--- static/js/script.js
+++ static/js/script.js
@@ -5,3 +5,17 @@ function openNav() {
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
+
+function toggleUserDetails() {
+  // console.log("Inside the toggleUserDetails() ");
+  // console.log(document.getElementById("userDetails").style.display);
+
+  // in the beginning "userDetails" has style display as an empty string ("")
+  // so we have added one more or conditio to check if it is an empty string
+  if (document.getElementById("userDetails").style.display == "none" || document.getElementById("userDetails").style.display == ""){
+    document.getElementById("userDetails").style.display = "block";
+  }
+  else if (document.getElementById("userDetails").style.display == "block"){
+    document.getElementById("userDetails").style.display = "none";
+  }
+}
